645afb140652fc748cc164e74dc16cae5ecbe8f4
jarnold84 <jarnold84@protonmail.com>
2025-08-18 13:58:43 -0400

    Create actor.ts

---
+++ src/actor.ts
@@ -0,0 +1,13 @@
+import { Actor } from 'apify';
+import { createCrawler } from './crawler.js';
+import type { ActorInput } from './types.js';
+
+await Actor.init();
+
+const input = await Actor.getInput<ActorInput>();
+if (!input || !input.startUrls?.length) throw new Error('Missing startUrls');
+
+const crawler = createCrawler(input);
+await crawler.run(input.startUrls);
+
+await Actor.exit();
